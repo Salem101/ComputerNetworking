c8de72071cba3bec513a28c57dd20f5f36c375e3
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2022-02-18 09:57:12 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,7 +7,7 @@ import sys
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
     # Prepare a server socket
-    serverSocket.bind(("", port))
+    serverSocket.bind(('',port))
     # Fill in start
     serverSocket.listen()
     # Fill in end
@@ -15,14 +15,14 @@ def webServer(port=13331):
     while True:
         # Establish the connection
         # print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept() # Fill in start      #Fill in end
+        connectionSocket, addr = serverSocket.accept() # Fill in start
+        #Fill in end
         try:
 
             try:
                 message = connectionSocket.recv(1024) # Fill in start    #Fill in end
                 filename = message.split()[1]
                 f = open(filename[1:])
-                outputdata = f.read() # Fill in start     #Fill in end
 
                 # Send one HTTP header line into socket.
                 # Fill in start
@@ -36,15 +36,15 @@ def webServer(port=13331):
                 connectionSocket.send("\r\n".encode())
                 connectionSocket.close()
             except IOError:
-        # Send response message for file not found (404)
-        # Fill in start
-        connectionSocket.send('404 Not Found')
-        # Fill in end
+                # Send response message for file not found (404)
+                # Fill in start
+                connectionSocket.send("404 Not Found")
+                # Fill in end
 
-        # Close client socket
-        # Fill in start
-        connectionSocket.close()
-        # Fill in end
+                # Close client socket
+                # Fill in start
+                connectionSocket.close()
+                # Fill in end
 
         except (ConnectionResetError, BrokenPipeError):
             pass
