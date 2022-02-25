7cec7cbb0c9e8c7674e7bf4ceabc4fbbfe1801c6
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2022-02-24 20:48:44 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -28,7 +28,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
-    clientSocket.send('MAIL FROM: <mail@nyu.edu>\r\n').encode())
+    clientSocket.send('MAIL FROM: <mail@nyu.edu>\r\n'.encode())
     recv2 = clientSocket.recv(1024).decode()
     # print(recv2)
     # if recv2[:3] != '250':
