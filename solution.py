ae4dcf703aacfc740b1a160bc8bbcbe40d4dbd8f
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2022-02-24 22:42:37 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -56,7 +56,6 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start
     clientSocket.send(msg.encode())
     recv = clientSocket.recv(1024).decode()
-
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
